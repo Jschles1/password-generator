b17f838913c6aad5cecf2fe78b6a9d67e48e99ef
Jschles1 <johnschlesinger5@gmail.com>
2023-07-11 14:56:51 -0400

    Accessibility fix attempt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -129,7 +129,7 @@ export default function Home() {
             {state.isCopied && (
               <p className="text-[1.125rem] text-lime mr-4">Copied!</p>
             )}
-            <button onClick={handleCopy} className="group">
+            <button onClick={handleCopy} className="group" aria-label="Copy">
               <svg width="21" height="24" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M20.341 3.091 17.909.659A2.25 2.25 0 0 0 16.319 0H8.25A2.25 2.25 0 0 0 6 2.25V4.5H2.25A2.25 2.25 0 0 0 0 6.75v15A2.25 2.25 0 0 0 2.25 24h10.5A2.25 2.25 0 0 0 15 21.75V19.5h3.75A2.25 2.25 0 0 0 21 17.25V4.682a2.25 2.25 0 0 0-.659-1.591ZM12.469 21.75H2.53a.281.281 0 0 1-.281-.281V7.03a.281.281 0 0 1 .281-.281H6v10.5a2.25 2.25 0 0 0 2.25 2.25h4.5v1.969a.282.282 0 0 1-.281.281Zm6-4.5H8.53a.281.281 0 0 1-.281-.281V2.53a.281.281 0 0 1 .281-.281H13.5v4.125c0 .621.504 1.125 1.125 1.125h4.125v9.469a.282.282 0 0 1-.281.281Zm.281-12h-3v-3h.451c.075 0 .147.03.2.082L18.667 4.6a.283.283 0 0 1 .082.199v.451Z"
@@ -164,6 +164,7 @@ export default function Home() {
             <div className="flex items-center mb-4 md:mb-[1.188rem]">
               <Checkbox
                 id="uppercase"
+                aria-labelledby="uppercase"
                 name="uppercase"
                 onClick={handleCheckboxChange}
               />
@@ -192,6 +193,7 @@ export default function Home() {
             <div className="flex items-center mb-4 md:mb-[1.188rem]">
               <Checkbox
                 id="numbers"
+                aria-labelledby="numbers"
                 name="numbers"
                 onClick={handleCheckboxChange}
               />
@@ -206,6 +208,7 @@ export default function Home() {
             <div className="flex items-center">
               <Checkbox
                 id="symbols"
+                aria-labelledby="symbols"
                 name="symbols"
                 onClick={handleCheckboxChange}
               />
@@ -242,6 +245,7 @@ export default function Home() {
             <button
               disabled={disableButton}
               onClick={handleSubmit}
+              aria-label="Generate"
               className="flex items-center text-[1.125rem] group w-full py-[1.25rem] justify-center transition-colors text-base uppercase bg-lime border-2 border-lime hover:bg-dark-gray hover:text-lime focus:bg-dark-gray focus:text-lime focus:outline-none disabled:bg-dark-gray disabled:text-lime disabled:cursor-not-allowed"
             >
               Generate
